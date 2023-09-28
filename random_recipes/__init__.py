3eef419d40722c56c04b2e97cf3f042bee0a5036
Ian Pearthree <52967928+ipear3@users.noreply.github.com>
2023-09-27 22:30:43 -0400

    implicit import instead of explicit

--- random_recipes/__init__.py
+++ random_recipes/__init__.py
@@ -1,2 +1,2 @@
 """Recipes to generate random instances of various types, beyond the scope of random."""
-from .recipes import Decimal, date, datetime, random_decimal, random_date, random_datetime
+from .recipes import *
