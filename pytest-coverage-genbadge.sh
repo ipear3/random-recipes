d690ee241eb1c47557bac652eaf61def87c12475
Ian Pearthree <52967928+ipear3@users.noreply.github.com>
2023-10-03 22:03:11 -0400

    dynamic versioning and move pytest, coverage, genbadge to pre-commit

---
+++ pytest-coverage-genbadge.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# Pytest via Coverage -> Tests badge
+echo "Running pytest via coverage..." &&
+coverage run --data-file reports/.coverage -m pytest --junitxml=reports/tests.xml &&
+echo "Running genbadge tests..." &&
+genbadge tests -i reports/tests.xml -o images/badges/tests.svg;
+# Coverage -> Coverage badge
+echo "Running coverage..." &&
+coverage xml --data-file reports/.coverage -o reports/coverage.xml --fail-under 100;
+echo "Running genbadge coverage..."
+genbadge coverage -i reports/coverage.xml -o images/badges/coverage.svg;
