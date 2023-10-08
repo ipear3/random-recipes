5174fc892c2aa0a2d597e3ba887c0755f979480b
Ian Pearthree <52967928+ipear3@users.noreply.github.com>
2023-10-08 14:36:38 -0400

    Fix FileNotFoundError on remote

--- tests/test_files.py
+++ tests/test_files.py
@@ -3,7 +3,9 @@ from random_recipes.files import *
 
 class TestRandomCSV:
     def test_random_csv_is_csv(self):
-        file = random_csv()
+        file = Path("data/random.csv")
+        file.parent.mkdir(exist_ok=True, parents=True)
+        file = random_csv(file=file)
         with open(file, newline="") as file:
             reader = csv.reader(file, dialect="unix")
             for row in reader:
